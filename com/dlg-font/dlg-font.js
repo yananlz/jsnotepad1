16ff35a1a61de17cd052071fcf6d929f960382d3
张亚楠 <44825312+yananlz@users.noreply.github.com>
2021-04-18 10:06:42 +0800

    Update dlg-font.js

--- com/dlg-font/dlg-font.js
+++ com/dlg-font/dlg-font.js
@@ -1,108 +1,82 @@
-let $dlgFont = new Dialog('font');
+var $dlgFont = (function() {
+  var $dlg = $(''
+      + '<div class="notepad-dlg-mask notepad-dlg-font">'
+        + '<div class="dialogbox notepad-dlgbox">'
+          + '<div class="notepad-dlg-titlebar">'
+            + '<p class="title">字体</p>'
+            + '<span class="close-btn">✖</span>'
+          + '</div>'
+          + '<div class="main notepad-dlg-main">'
+            + '<div class="font-family"><p>字体(F):</p></div>'
+            + '<div class="font-style"><p>字形(Y):</p></div>'
+            + '<div class="font-size"><p>大小(S):</p></div>'
+            + '<fieldset class="sample">'
+              + '<legend>示例</legend>'
+              + '<p>AaBbYyZz</p>'
+            + '</fieldset>'
+            + '<div class="script">'
+              + '<label>'
+                + '脚本(R):<br>'
+                + '<select>'
+                  + '<option value="西欧语言">西欧语言</option>'
+                  + '<option value="中文 GB2312">中文 GB2312</option>'
+                + '</select>'
+              + '</label>'
+            + '</div>'
+            + '<input class="btn-ok btn" type="button" value="确定">'
+            + '<input class="btn-cancel btn" type="button" value="取消">'
+          + '</div>'
+        + '</div>'
+      + '</div>');
 
-((dlg) => {
-  let content = `
-    <div class="font-family"><p>字体(F):</p></div>
-    <div class="font-style"><p>字形(Y):</p></div>
-    <div class="font-size"><p>大小(S):</p></div>
-    <fieldset class="sample">
-      <legend>示例</legend>
-      <p class="sample-txt">AaBbYyZz</p>
-    </fieldset>
-    <div class="script">
-      <label>
-        脚本(R):<br>
-        <select>
-          <option value="西欧语言">西欧语言</option>
-          <option value="中文 GB2312">中文 GB2312</option>
-        </select>
-      </label>
-    </div>
-    <input class="btn-ok btn" type="button" value="确定">
-    <input class="btn-cancel btn" type="button" value="取消">`;
-
-  let $dlg       = dlg.generate(content, '字体');
-
-  let $btnOk     = $dlg.find('.btn-ok'),
+  var $btnOk = $dlg.find('.btn-ok'),
+      $btnClose = $dlg.find('.close-btn'),
       $btnCancel = $dlg.find('.btn-cancel'),
-      $sample    = $dlg.find('.sample-txt');
+      $titleBar = $dlg.find('.notepad-dlg-titlebar');
 
-  let fonts = ['Agency FB', 'Algerian', 'Arial', 'Arial Rounded MT', 'Axure Handwriting', 'Bahnschrift', 'Baskerville Old Face', 'Bauhaus 93', 'Bell MT', 'Berlin Sans FB', 'Bernard MT', 'BlackAdder ITC'],
-      styles = ['常规', '斜体', '粗体', '粗斜体'],
-      sizes = ['8', '9', '10', '11', '12', '14', '16', '18', '20', '22', '24', '26', '28', '36', '48', '72'];
+  function init() {
+    var fonts = ['Agency FB', 'Algerian', 'Arial', 'Arial Rounded MT', 'Axure Handwriting', 'Bahnschrift', 'Baskerville Old Face', 'Bauhaus 93', 'Bell MT', 'Berlin Sans FB', 'Bernard MT', 'BlackAdder ITC'],
+        styles = ['常规', '斜体', '粗体', '粗偏斜体'],
+        sizes = ['8', '9', '10', '11', '12', '14', '16', '18', '20', '22', '24', '26', '28', '36', '48', '72'];
 
-  let cfg = {
-    family:    'Arial',
-    style:     '常规',
-    size:      '16',
-    okHandler: null
-  };
-
-  let sample = () => {
-    $sample.css({ 'font-family': cfg.family, 'font-size': cfg.size + 'pt' });
-    np.setFontStyle($sample, cfg.style);
-  };
-
-  let initList = () => {
-    let lstFamily = new comList();
-    lstFamily.show({
+    var l1 = new comList();
+    l1.show({
       container: '.notepad-dlg-font .font-family',
-      width:     '176px',
-      list:      fonts,
-      select:    fonts.indexOf(cfg.family),
-      isFont:    true,
-      selectHandler: (e) => {
-        cfg.family = fonts[e];
-        sample();
-      }
+      width: '176px',
+      list: fonts,
+      isFont: true,
+      selectHandler: function(e) { console.log(fonts[e]); }
     });
 
-    let lstStyle = new comList();
-    lstStyle.show({
-      container:   '.notepad-dlg-font .font-style',
-      width:       '132px',
-      list:        styles,
-      select:      styles.indexOf(cfg.style),
+    var l2 = new comList();
+    l2.show({
+      container: '.notepad-dlg-font .font-style',
+      select: 3,
+      width: '132px',
+      list: styles,
       isFontStyle: true,
-      selectHandler: (e) => {
-        cfg.style = styles[e];
-        sample();
-      }
+      selectHandler: function(e) { console.log(styles[e]); }
     });
 
-    let lstSize = new comList();
-    lstSize.show({
+    var l3 = new comList();
+    l3.show({
       container: '.notepad-dlg-font .font-size',
-      width:     '64px',
-      list:      sizes,
-      select:    sizes.indexOf(cfg.size),
-      selectHandler: (e) => {
-        cfg.size = sizes[e];
-        sample();
-      }
+      width: '64px',
+      list: sizes,
+      selectHandler: function(e) { console.log(sizes[e]); }
     });
+  }
 
-    sample();
-  };
-
-  dlg.show = (conf) => {
-    $.extend(cfg, conf);
-
+  function destory() { $dlg.remove(); }
+  function show() {
     $('body').append($dlg);
-    dlg.init();
-    initList();
+    init();
+    $dlg.find('.dialogbox').draggable({handle: $titleBar});
 
-    $btnCancel.click(dlg.destory);
-    $btnOk.click(() => {
-      cfg.okHandler({
-        family: cfg.family,
-        style:  cfg.style,
-        size:   cfg.size
-      });
+    $btnClose.click(destory);
+    $btnCancel.click(destory);
+    $btnOk.click(destory);
+  }
 
-      dlg.destory();
-    });
-
-    $dlg.click((e) => e.stopPropagation());
-  };
-})($dlgFont);
+  return {show: show};
+}());
